fa67ab1c3f549af233506d5fce431542650ef475
Rafael Dias <rafadm2305@gmail.com>
2024-08-20 12:30:36 -0300

    💄 ui: Updating the UI new pages

--- renderer/pages/sala.jsx
+++ renderer/pages/sala.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
+import Runner from "../components/runner.js";
 
 export default function NextPage() {
   return (
@@ -9,9 +10,23 @@ export default function NextPage() {
         <title>Sala</title>
       </Head>
       <div>
-        <p>
+        {/* <p>
           Sala Nova - <Link href="/home">Go to home page</Link>
-        </p>
+        </p> */}
+        <h2>Cópia - Sala Nova</h2>
+        <div className="card">
+          <h3>Tutorial</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+        <Runner></Runner>
       </div>
     </React.Fragment>
   )
