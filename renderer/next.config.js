9cc80081fb00aa795cb995e20b8c954006951574
Rafael Dias <rafadm2305@gmail.com>
2024-10-24 23:05:19 -0300

    🚧 wip: Work in progress

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -1,16 +1,17 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   // Comment out or remove the output export setting
-  output: 'export',
+  output: "export",
   // output: 'standalone',
 
-  distDir: process.env.NODE_ENV === 'production' ? '../app' : '.next',
+  distDir: process.env.NODE_ENV === "production" ? "../app" : ".next",
   trailingSlash: true,
-  
+
   images: {
     unoptimized: true,
   },
   webpack: (config) => {
-    return config
-  }
-}
+    config.cache = false;
+    return config;
+  },
+};
