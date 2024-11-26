a128ee7ceae00ddb583dee23631bdf685222a432
rdm10br <rafadm2305@gmail.com>
2024-11-26 14:35:25 -0300

    🚀 enhancement: BQ tutorial [pull-request]

--- renderer/pages/bot/bq.jsx
+++ renderer/pages/bot/bq.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
-import Head from 'next/head'
-import Link from 'next/link'
+import React from "react";
+import Head from "next/head";
 import Runner from "../../components/Runner/runner";
 
 export default function NextPage() {
@@ -14,17 +13,27 @@ export default function NextPage() {
         <div className="card-tutorial">
           <h3>Tutorial</h3>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            Para a utilização deste robô, é necessário apenas que, nas variáveis
+            de ambiente, exista a variável "ID_REPOSITORIO_BQ" com o ID da
+            sala desejada. Além disso, durante a execução do robô, ele
+            solicitará quais Bancos de Questões devem ser utilizados e se são do
+            tipo "Junção". Após essa etapa, ele seguirá com a criação conforme
+            selecionado previamente.
+          </p>
+          <p>
+            Qualquer erro ou demora pode ser reportado por meio do Feedback
+            abaixo. Isso abrirá uma issue no GitHub do repositório, que será
+            tratada e resolvida em breve. Recomendo que o feedback seja enviado
+            logo após o problema ocorrer.
+          </p>
+          <p>
+            Em caso de instabilidades na plataforma, pode ocorrer um erro de
+            timeout. Nesse caso, basta executar o robô novamente, e ele
+            continuará do ponto onde parou.
           </p>
         </div>
-        <Runner script='Main_BQ.py'></Runner>
+        <Runner script="Main_BQ.py"></Runner>
       </div>
     </React.Fragment>
-  )
+  );
 }
