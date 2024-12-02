046050c1f36bd558256c453a9b0360ef56fa8f11
rdm10br <rafadm2305@gmail.com>
2024-12-02 15:49:26 -0300

    🚀 enhancement:  updater page [pull-request]

--- main/ipc/autoUpdater.js
+++ main/ipc/autoUpdater.js
@@ -15,6 +15,7 @@ export function checkForUpdates(mainWindow) {
   // Notify renderer when an update is available
   autoUpdater.on("update-available", () => {
     mainWindow.webContents.send("update_available");
+    mainWindow.webContents.send("navigate", "/update-progress");
   });
 
   // Show dialog and prompt to restart when update is downloaded
